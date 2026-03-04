8964e3640a5ac6369f9cab53ae7835b39e31cc7e
Wolfgang Vieregg <wollux@me.com>
2026-03-04 23:23:09 +0100

    Fix SHA256 checksum for v0.8.1 DMG

--- Casks/pastejack.rb
+++ Casks/pastejack.rb
@@ -1,6 +1,6 @@
 cask "pastejack" do
   version "0.8.1"
-  sha256 "64e5a2591b4a053745f2db3164794bdde433febe9c4120c75960fc1482b76789"
+  sha256 "52fc5672305e299e4ce174f27a35b0be42cd5e1e8958d015ead14188a3219bd3"
   url "https://github.com/wollux/PasteJack/releases/download/v#{version}/PasteJack-#{version}.dmg"
   name "PasteJack"
   desc "Paste clipboard contents as simulated keystrokes — bypass paste-blocking"
