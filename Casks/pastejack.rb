1ad52c70f4efe35249541ed55a3a8362955831a5
Wolfgang Vieregg <wollux@me.com>
2026-03-04 09:11:08 +0000

    Update PasteJack to 0.3.0

--- Casks/pastejack.rb
+++ Casks/pastejack.rb
@@ -1,6 +1,6 @@
 cask "pastejack" do
-  version "0.2.3"
-  sha256 "0fad8a80ed53eecb1fc4049e201fa131fd5a73005fbbc1c66cf7cc8010a0e39f"
+  version "0.3.0"
+  sha256 "3dcecc98b3b82e231d203e6a39fcc62d3fbf70fe8edf840bc043fb39d96414cb"
   url "https://github.com/wollux/PasteJack/releases/download/v#{version}/PasteJack-#{version}.dmg"
   name "PasteJack"
   desc "Paste clipboard contents as simulated keystrokes — bypass paste-blocking"
